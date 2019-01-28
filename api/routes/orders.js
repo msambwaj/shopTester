c2131e8ace2d285faab57fbe3d635dc4e3b99b07
john <john@sylvestres-MacBook-Air.local>
2019-01-28 15:08:23 +0800

    creating and writing initial codes

---
+++ api/routes/orders.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'orders were fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'orders were created'
+    });
+});
+
+router.get('/:orderId', (req, res, next) =>{
+    const id = req.params.orderId;
+    if (id === 'gauni'){
+        res.status(200).json({
+            message : 'umepata gauni jipya',
+            id : id
+        });
+    }else{
+        res.status(200).json({
+            message : 'umekosea njia'
+        });
+    }
+});
+
+router.patch('/:orderId', (req, res, next) =>{
+   // const id = req.params.productId;
+        res.status(201).json({
+            message : 'order updated',
+        });
+    
+
+});
+
+router.delete('/:orderId', (req, res, next) =>{
+    // const id = req.params.productId;
+         res.status(201).json({
+             message : 'order deleted',
+         });
+     
+ 
+ });
+
+module.exports = router;
---
+++ api/routes/products.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling the GET request'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling the POST request'
+    });
+});
+
+router.get('/:productId', (req, res, next) =>{
+    const id = req.params.productId;
+    if (id === 'gauni'){
+        res.status(200).json({
+            message : 'umepata gauni jipya',
+            id : id
+        });
+    }else{
+        res.status(200).json({
+            message : 'umekosea njia'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) =>{
+   // const id = req.params.productId;
+        res.status(201).json({
+            message : 'details updated',
+        });
+    
+
+});
+
+router.delete('/:productId', (req, res, next) =>{
+    // const id = req.params.productId;
+         res.status(201).json({
+             message : 'details deleted',
+         });
+     
+ 
+ });
+
+module.exports = router;
