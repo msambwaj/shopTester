431b5c258a3f77252e6d23f979c19908df900657
john <john@sylvestres-MacBook-Air.local>
2019-01-28 18:55:04 +0800

    add body-parser and nodemon

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 
+    'Orign, X-Requested_With, Content_type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Method',
+         'PUT, POST, PATCH, DELETE, GET');
+         return res.status(200).json({
+
+         });
+    }
+    next();
+});
+
+//Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
